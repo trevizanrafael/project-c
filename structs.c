52db6377e7227819957b0c1392e499022c6d22fe
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-01-18 09:56:56 -0300

    Create structs.c

---
+++ structs.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structs para o projeto
+
+typedef struct
+{
+	int dia;
+	int mes;
+	int ano;
+} data;
+
+typedef struct
+{
+	int hora;
+	int min;
+} hora;
+
+typedef struct
+{
+	int codigo_dis;
+	char disciplina[100];
+	float notas[4];
+	float media;
+} disciplina;
+
+typedef struct
+{
+	int ra;
+	char nome[100];
+	td_data matricula;
+	td_disciplina disciplinas[4];
+	char email[100];
+} aluno;
