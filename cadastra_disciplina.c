f6ab1b36bcfd2a164d1d1855562b622bc26b05b9
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-02-10 17:10:15 -0300

    Update cadastra_disciplina.c

--- cadastra_disciplina.c
+++ cadastra_disciplina.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
-
 typedef struct{
-    int codigo_dis;
-    char disciplina[100];
-    float notas[4];
-    float media;
-} td_disciplina;
+        int codigo_dis;
+        char disciplina[100];
+        float notas[4];
+        float media;
+    } disciplina;
 
-void cadastra_disciplina ()
+void cadastra_disciplina(disciplina disciplinas[], int num_disciplinas){
+    int i;
+    for(i=0;i<num_disciplinas;i++)
+    {
+        printf("Digite o código da disciplina: ");
+        scanf("%d", &disciplinas[i].codigo_dis);
+        printf("Digite o código da disciplina: ");
+        scanf(" %[^\n]", disciplinas[i].disciplina); 
+    }
+}
+int main() {
+    disciplina disciplinas[1];
+    cadastra_disciplina(disciplinas,1);
+    return 0;
+}
